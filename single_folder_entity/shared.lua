8fd67c872e39f1749b6fc132d185c3fc34488683
Артем <bydlo.codd@bk.ru>
2023-08-10 18:09:25 +0300

    typo fix

--- single_folder_entity/shared.lua
+++ single_folder_entity/shared.lua
@@ -1,5 +1,5 @@
 --[[
-    The second to develop an entity is to create a directory in `entities/`. 
+    The second way to develop an entity is to create a directory in `entities/`. 
     By default `init.lua` being called serverside and `cl_init.lua` clientside. Any other files
     inside the folder must be included. That's why you see `AddCSLuaFile('shared.lua')` and `include('shared.lua')`
     The folrder name defines the class of your entity (https://wiki.facepunch.com/gmod/Entity:GetClass).
